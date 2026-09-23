d90b02f835ad29d2d50fb8441caa72e76bbb623b
emiliacurtis716 <emilia.curtis716@gmail.com>
2026-09-23 15:25:20 +0100

    Implement transition step

--- app/src/main/java/com/example/lemonadeapp/MainActivity.kt
+++ app/src/main/java/com/example/lemonadeapp/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonadeapp.ui.theme.LemonadeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,10 +44,47 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun LemonadeApp() {
-
+    LemonadeStep(Modifier
+        .fillMaxSize()
+        .wrapContentSize())
 }
 
 @Composable
-fun LemonadeStep() {
-
+fun LemonadeStep(modifier: Modifier = Modifier) {
+    var step by remember { mutableStateOf(1) }
+    val imageResource = when (step % 4) {
+        1 -> R.drawable.lemon_tree
+        2 -> R.drawable.lemon_squeeze
+        3 -> R.drawable.lemon_drink
+        else -> R.drawable.lemon_restart
+    }
+    val stringResource = when (step % 4) {
+        1 -> R.string.lemon_tree
+        2 -> R.string.lemon
+        3 -> R.string.glass_of_lemonade
+        else -> R.string.empty_glass
+    }
+    val stepFunction = {
+        step++
+    }
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = { stepFunction() },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Green),
+            shape = RoundedCornerShape(20)
+        ) {
+            Image(
+                painter = painterResource(imageResource),
+                contentDescription = step.toString()
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            stringResource(stringResource),
+            fontSize = 18.sp
+        )
+    }
 }
